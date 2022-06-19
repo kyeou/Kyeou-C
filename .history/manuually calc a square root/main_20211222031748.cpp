b79353252e2c74ccbcb05cc79f27aeacdcc42920
Kyeou <77829365+kyeou@users.noreply.github.com>
2022-06-18 18:05:01 -0700

    Delete .history directory

--- .history/manuually calc a square root/main_20211222031748.cpp
+++
@@ -1,9 +0,0 @@
-#define forL(x) for(int i = 0; i < x; i++)
-#define printS(s) std::cout << s << std::endl;
-#define randK(x) 1 + (rand() % x);
-#define ifS(a, b) if (a) b;
-#define ifES(a, b, c, d) if (a) {b} else (c) {d} ;
-
-int main() {
-    
-}
