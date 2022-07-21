ad2fd614126773f8c6a636132ef8fd0bdae2a19b
kyeou <christianjarmon630@gmail.com>
2022-07-20 19:17:45 -0700

    ...

--- collatz/collatz.c
+++ collatz/collatz.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    FILE *fp = fopen("D://results.txt", "w");
-    for (int i = 0; i < 20000001; i++)
+    FILE *fp = fopen("results.txt", "w");
+    for (int i = 1; i < 1000; i++)
     {
         unsigned long number = i;
         fprintf(fp, "%d: ", number);
-        printf("%d\n", number);
+        // printf("%d\n", number);
         while (number != 1)
         {
             if (number % 2 == 0)
